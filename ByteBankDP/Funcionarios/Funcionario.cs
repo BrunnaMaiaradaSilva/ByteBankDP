84922d710776509d5b1d9fe51f420d3dc3237ca1
BrunnaMaiaradaSilva <silva.brunna@gmail.com>
2020-03-20 00:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBankDP/Funcionarios/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankDP.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 = funcionário
+        // 1 = diretor
+        private int _tipo;
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+
+    }
+}
---
+++ ByteBankDP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBankDP.Funcionarios;
+
+namespace ByteBankDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario brunna = new Funcionario(1);
+
+            brunna.Nome = "Brunna";
+            brunna.CPF = "445.654.789-25";
+            brunna.Salario = 4890;
+
+            Console.WriteLine(brunna.Nome);
+
+            Console.WriteLine(brunna.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
