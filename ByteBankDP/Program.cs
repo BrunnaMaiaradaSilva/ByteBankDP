0ff4634d6cf9f29150648bfe2bc18ec86ebc22bb
BrunnaMaiaradaSilva <silva.brunna@gmail.com>
2020-03-24 17:54:51 -0300

    Implementação de classes abstratas

--- ByteBankDP/Program.cs
+++ ByteBankDP/Program.cs
@@ -11,33 +11,27 @@ namespace ByteBankDP
     {
         static void Main(string[] args)
         {
-            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
-
-            Funcionario brunna = new Funcionario(4890,"445.654.789-25");
-
-            brunna.Nome = "Brunna";
-           
-            gerenciador.Registrar(brunna);
-
-            Diretor roberta = new Diretor(4890, "445.681.269-28");
-            roberta.Nome = "Roberta";
-
-            gerenciador.Registrar(roberta);
-
-            Console.WriteLine(brunna.Nome);
-            Console.WriteLine(brunna.GetBonificacao());
-
-            Console.WriteLine(roberta.Nome);
-            Console.WriteLine(roberta.GetBonificacao());
-
-            Console.WriteLine(gerenciador.GetTotalBonificacao());
-
-            brunna.AumentarSalário();
-            Console.WriteLine("Novo salário: " + brunna.Salario);
-
-            Console.WriteLine(Funcionario.TotalDeFuncionarios);
+            CalcularBonificacao();
 
             Console.ReadLine();
         }
+
+        public static void CalcularBonificacao()
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Designer designer = new Designer(4000, "777.777.777-99");
+            Diretor diretor = new Diretor(8000, "777.777.777-99");
+            GerenteDeConta gerenteDeConta = new GerenteDeConta(7000, "777.777.777-99");
+            Auxiliar auxiliar = new Auxiliar(2000, "777.777.777-99");
+
+            gerenciadorBonificacao.Registrar(designer);
+            gerenciadorBonificacao.Registrar(auxiliar);
+            gerenciadorBonificacao.Registrar(diretor);
+            gerenciadorBonificacao.Registrar(gerenteDeConta);
+
+            Console.WriteLine("Total de bonificações " + 
+                gerenciadorBonificacao.GetTotalBonificacao());
+        }
     }
-}
+}   
